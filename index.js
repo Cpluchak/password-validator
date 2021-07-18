9d0a36d7470d419f2c50fed60322ae4928dd3456
CodyPluchak <pbkid21@yahoo.com>
2021-07-18 19:01:28 -0400

    ran test and all passed

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-function validatePassword(password) {
+
+
+
+function checkForLowerCase(password){
+    for (var i = 0; i < password.length; i++){
+        if (password[i] >= "a" && password[i] <="z"){
+            return true
+        }
+    }return false}
+
+function checkForLength(password){
+    if (password.length < 8) {
+        return false
+    }
+    return true
+}
+
+function checkForUpperCase(password) {
+    for (var i = 0; i < password.length; i++){
+        if (password[i] >= "A" && password[i] <="Z"){
+            return true
+        }
+    }
+    return false
+}
+
+
+function checkForNumber(password) {
+    for (var i = 0; i < password.length; i++){
+        if (password[i] >= 0 && password[i] <= 9){
+            return true
+        }
+    }
+    return false
+}
+
+function checkSpecialCharacter(password) {
+    var specialcharacters ="!@#$%^&*()-_"
+    for (var i = 0; i < password.length; i++){
+        if (specialcharacters.includes(password[i])){
+            return true
+        }
+    }
+    return false
+    }
+
+    function validatePassword(password) {
     let lowerCase = false
 let passwordLength = false
 let upperCase = false
@@ -11,51 +57,6 @@ let checkCharacter = false
     checkCharacter = checkSpecialCharacter(password)
     return lowerCase && passwordLength && upperCase && checkNumber && checkCharacter
 }
-
-
-function checkForLowerCase(password){
-    for (var i = 0; i < password.length; i++){
-        if (password[i] >= "a" && password[i] <="z"){
-            return true
-        }
-        return false
-    }
-
-function checkForLength(password){
-    if (password.length < 8) {
-        return false
-    }
-    return true
-}
-
-function checkForUpperCase(password) {
-    for (var i = 0; i < passwordLength; i++){
-        if (password[i] >= "A" && password[i] <="Z"){
-            return true
-        }
-    }
-}
-return false
-}
-
-function checkForNumber(password) {
-    for (var i = 0; i < passwordLength; i++){
-        if (password[i] >= 0 && password[i] <= 9){
-            return true
-        }
-    }
-    return false
-}
-
-function checkSpecialCharacter(password) {
-    var specialcharacters ="!@#$%^&*()-_"
-    for (var i = 0; i < passwordLength; i++){
-        if (specialcharacters.includes(password[i])){
-            return true
-        }
-    }
-    return false
-    }
-
+module.exports=validatePassword
 
 
