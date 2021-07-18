6dd7965a88cdb04a6b17883f9e4ee8e27f1265e2
CodyPluchak <pbkid21@yahoo.com>
2021-07-18 17:25:46 -0400

    added validatepassword function

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function validatePassword(password) {
-
+    let lowerCase = false
+let passwordLength = false
+let upperCase = false
+let checkNumber = false
+let checkCharacter = false
+    passwordLength = checkForLength(password)
+    lowerCase = checkForLowerCase(password)
+    upperCase = checkForUpperCase(password)
+    checkNumber = checkForNumber(password)
+    checkCharacter = checkSpecialCharacter(password)
+    return lowerCase && passwordLength && upperCase && checkNumber && checkCharacter
 }
+module.exports = validatePassword
